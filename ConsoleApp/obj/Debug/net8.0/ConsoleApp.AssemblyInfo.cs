e46f92bb725366a849b4ac4b73598e058d090961
RealBlueSwan <nobrunner@hotmail.com>
2024-07-20 20:06:29 +0200

    Moved some files for github

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4460eb85877b7e30380b9c3466f4eb6c29da6d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4046ce1a4d9a1c616d2b5ffff109301be8c07e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
